ad41aa786a570983acbb96609164ecb43d0477eb
Stew404 <artembotov@mail.ru>
2020-12-27 19:58:25 +0300

    added mobile menu

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const headerButton = document.querySelector(".header__button");
+      headerMenuClose = document.querySelector(".header__menu-close");
+      headerMenu = document.querySelector(".header__menu_mobile");
+
+      let translateX = 100;
+      headerButton.addEventListener("click", () =>{
+         headerMenu.style.display = "flex";
+
+         let animationOpen = setInterval(() => {
+            
+            translateX -= 3;
+            headerMenu.style.transform = "translateX(" + translateX + "%)";
+            if (translateX <= 0) {
+               clearInterval(animationOpen);
+               headerMenu.style.transform = "translateX(0%)"
+            }
+         }, 1);
+      });
+
+      headerMenuClose.addEventListener("click", () =>{
+         
+         let animationClose = setInterval(() => {
+            translateX += 3;
+            headerMenu.style.transform = "translateX(" + translateX + "%)";
+            if (translateX >= 100) {
+               clearInterval(animationClose);
+               headerMenu.style.transform = "translateX(100%)";
+               headerMenu.style.display = "none";
+            }
+         }, 1);
+
+
+      });
+
